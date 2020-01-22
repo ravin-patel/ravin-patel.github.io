067c4f2618115c018916964019fac68a3b25bc7f
Ravin <ravin_p21@hotmail.com>
2020-01-22 00:46:15 -0500

    new website

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+  imagebaseurl: "https://ravinpatel.github.io/",
+  name: "ravin patel",
+  role: "Software Engineer",
+  linkedinId: "ravin-patel",
+  skypeid: "",
+  roleDescription:
+    "I dabble in various parts of development and always passionate to learn about new technologies. ",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "http://linkedin.com/in/ravin-patel",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "ravin-patel.github.io",
+      className: "fa fa-github"
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/",
+      className: "fa fa-twitter"
+    }
+  ],
+  aboutme:
+    "Ryerson Univeristy Computer Science Graduate, currently working as a Software Engineer ",
+  address: "Toronto,On",
+  website: "https://ravin-patel.github.io",
+  education: [
+    {
+      UniversityName: "Ryerson University",
+      specialization: "Bachelors of Honours Science : ComputerScience",
+      MonthOfPassing: "May",
+      YearOfPassing: "2018",
+      Achievements: ""
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Ceridian Dayforce",
+      specialization: "Software Engineering Intern",
+      MonthOfLeaving: "Sept",
+      YearOfLeaving: "2017",
+      Achievements: " - "
+    },
+    {
+      CompanyName: "Publicis.Sapient",
+      specialization: "Software Engineer - Dev Ops",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2019",
+      Achievements: " - "
+    },
+    {
+      CompanyName: "Publicis.Sapient",
+      specialization: "Software Engineer - AEM Backend",
+      MonthOfLeaving: "Present",
+      YearOfLeaving: "2020",
+      Achievements: " - "
+    }
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5"
+    },
+    {
+      skillname: "CSS"
+    },
+    {
+      skillname: "Reactjs"
+    }
+  ],
+  portfolio: [
+    {
+      name: "AWS S3 CLI",
+      description: "",
+      imgurl: "images/portfolio/s3.png"
+    },
+    {
+      name: "Auto Complete",
+      description: "",
+      imgurl: "images/portfolio/autocomplete.png"
+    },
+    {
+      name: "Conways Game Of Life Simulation",
+      description: "",
+      imgurl: "images/portfolio/gol.gif"
+    },
+    {
+      name: "PacMan",
+      description: "mobileapp",
+      imgurl: "images/portfolio/pacman.png"
+    }
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
+    }
+  ]
+};
+
+export default resumeData;
