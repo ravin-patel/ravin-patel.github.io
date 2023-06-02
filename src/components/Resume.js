1a6e5e9943b1f0f55b8e7facfa9ad849ff85e6a4
Ravin Patel <ravin_p21@hotmail.com>
2023-06-02 10:44:32 -0400

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -62,34 +62,7 @@ export default class Resume extends Component {
           </div>
         </div>
 
-        <div className="row skill">
-          <div className="three columns header-col">
-            <h1>
-              <span>Skills</span>
-            </h1>
-          </div>
-
-          <div className="nine columns main-col">
-            <h5>Frameworks/Tools/Technologies: </h5>
-            <p>{resumeData.technologies}</p>
-
-            <div className="bars">
-              <ul className="skills">
-                {resumeData.skills &&
-                  resumeData.skills.map(item => {
-                    return (
-                      <li>
-                        <span
-                          className={`bar-expand ${item.skillName.toLowerCase()}`}
-                        ></span>
-                        <em>{item.skillName === "cSharp" ? 'C#' : item.skillName}</em>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
-          </div>
-        </div>
+        
       </section>
     );
   }
